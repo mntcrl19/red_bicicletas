60eb81c3e0c6d50266e4f5042587fb451c79d577
Guillermo Montecoral <guillermo.montecoral@gmail.com>
2020-10-12 15:54:35 -0300

    agregar, editar y eliminar bicicletas, utilizacion del model view controller

---
+++ red_bicicletas/routes/bicicletas.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var bicicletaController = require('../Controllers/bicicleta');
+
+router.get('/', bicicletaController.bicicleta_list);
+router.get('/create', bicicletaController.bicicleta_create_get);
+router.get('/:id/update', bicicletaController.bicicleta_update_get);
+router.post('/create', bicicletaController.bicicleta_create_post);
+router.post('/:id/delete', bicicletaController.biccleta_delete_post);
+router.post('/:id/update', bicicletaController.bicicleta_update_post);
+
+module.exports = router;
