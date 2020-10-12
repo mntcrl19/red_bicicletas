c9703e7576222a0f37a691dd26c1c203774d1686
Guillermo Montecoral <guillermo.montecoral@gmail.com>
2020-10-12 16:57:46 -0300

    api de bicicletas

--- red_bicicletas/public/javascripts/map.js
+++ red_bicicletas/public/javascripts/map.js
@@ -15,7 +15,19 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map)
 
 //Marcadores en el mapa
-L.marker([-34.9041727, -56.197949]).addTo(map); //Centro - Montevideo
-L.marker([-34.9066979, -56.2120628]).addTo(map); //Ciudad Vieja - Montevideo
-L.marker([-34.894229, -56.1739111]).addTo(map); //Tres Cruces - Montevideo
-L.marker([-34.8971034, -56.1471212]).addTo(map); //Buceo - Montevideo
+//L.marker([-34.9041727, -56.197949]).addTo(map); //Centro - Montevideo
+//L.marker([-34.9066979, -56.2120628]).addTo(map); //Ciudad Vieja - Montevideo
+//L.marker([-34.894229, -56.1739111]).addTo(map); //Tres Cruces - Montevideo
+//L.marker([-34.8971034, -56.1471212]).addTo(map); //Buceo - Montevideo
+
+//Llamada AJAX
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result) {
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, { title: bici.id }).addTo(map);
+        });
+    }
+})
